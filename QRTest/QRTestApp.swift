a72a4ec1743eead7dcacb294dd82d496f41c9508
Jinsan Kim <gkjs1129@gmail.com>
2022-06-12 00:40:21 +0900

    Initial Commit

---
+++ QRTest/QRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRTestApp.swift
+//  QRTest
+//
+//  Created by Jinsan Kim on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct QRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
